f45cd4de4fc1d5201897323d5b5c48143de5cd80
unknown <hippoking1@gmail.com>
2026-08-20 12:12:50 +0800

    feat: 啟用 Android APK 熱更新模式 (自動同步 GitHub Pages 最新程式碼)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,8 @@ const config: CapacitorConfig = {
   appName: '智慧記帳',
   webDir: 'dist',
   server: {
+    url: 'https://hippoking1.github.io/-app/',
+    cleartext: true,
     androidScheme: 'https'
   }
 };
